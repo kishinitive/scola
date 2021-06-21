35b83eb8361b0a51210f04a4d4b78c4ff78c75f0
kishinitive <gips.komura@gmail.com>
2021-06-21 23:02:39 +0900

    記事モデルの作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,64 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
   public function index()
   {
-    $articles = [
-      (object) [
-        'id' => 1,
-        'title' => 'タイトル',
-        'body' => '本文1',
-        'created_at' => now(),
-        'user' => (object) [
-          'id' => 1,
-          'name' => 'ユーザー名1',
-        ],
-      ],
-      (object) [
-        'id' => 2,
-        'title' => 'タイトル2',
-        'body' => '本文2',
-        'created_at' =>now(),
-        'user' => (object) [
-          'id' => 2,
-          'name' => 'ユーザー名2',
-        ],
-      ],
-      (object) [
-        'id' => 3,
-        'title' => 'タイトル3',
-        'body' => '本文3',
-        'created_at' => now(),
-        'user' => (object) [
-          'id' => 3,
-          'name' => 'ユーザー名3'
-        ],
-      ],
-            (object) [
-        'id' => 4,
-        'title' => 'タイトル4',
-        'body' => '本文4',
-        'created_at' => now(),
-        'user' => (object) [
-          'id' => 4,
-          'name' => 'ユーザー名4',
-        ],
-      ],
-      (object) [
-        'id' => 5,
-        'title' => 'タイトル5',
-        'body' => '本文5',
-        'created_at' =>now(),
-        'user' => (object) [
-          'id' => 5,
-          'name' => 'ユーザー名5',
-        ],
-      ],
-    ];
+
+    $articles = Article::all()->sortByDesc('created_at');
 
     return view('articles.index', ['articles' => $articles]);
   }
