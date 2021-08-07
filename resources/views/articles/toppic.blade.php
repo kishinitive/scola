93a1fc8e27441f478cec60714e7ca3bfc4704ba0
kishinitive <gips.komura@gmail.com>
2021-08-07 14:55:00 +0900

    imageディレクトリをpublic配下に移動

--- resources/views/articles/toppic.blade.php
+++ resources/views/articles/toppic.blade.php
@@ -1,6 +1,6 @@
   @guest
   <div class="card-body pt-0">
-    <img class='w-100 mb-3' src="{{ asset('storage/scolatop.005.png') }}">
+    <img class='w-100 mb-3' src="{{ asset('resource/image/scolatop.png') }}">
   </div>
   @endguest
 
