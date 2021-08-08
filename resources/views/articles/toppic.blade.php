b3c90ee4dea4f7c2a4fc77ba3ae2e58e69ef602e
kishinitive <gips.komura@gmail.com>
2021-08-08 11:03:09 +0900

    トップページを編集
    使い方の画像を追加

--- resources/views/articles/toppic.blade.php
+++ resources/views/articles/toppic.blade.php
@@ -2,5 +2,9 @@
   <div class="card-body pt-0">
     <img class='w-100 mb-3' src="{{ asset('image/scolatop.png') }}">
   </div>
+  <div class="card-body pt-0">
+    <img class='w-100 mb-3' src="{{ asset('image/howtouse.png') }}">
+  </div>
+
   @endguest
 
