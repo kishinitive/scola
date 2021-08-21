a0fa25837a01ab3e0fc980d1f33fc00731bf8889
kishinitive <gips.komura@gmail.com>
2021-08-21 20:03:47 +0900

    コメントを修正
    コメントのユーザー名からユーザーページに正しく飛べるように修正

--- resources/views/comments/card.blade.php
+++ resources/views/comments/card.blade.php
@@ -1,11 +1,11 @@
 <div class="card mt-3">
   <div class="card-body d-flex flex-row">
-    <a href="{{ route('users.show',['name' => $article->user->name]) }}" class="text-dark">
+    <a href="{{ route('users.show',['name' => $comment->user->name]) }}" class="text-dark">
       <i class="fas fa-user-circle fa-3x mr-1"></i>
     </a>
     <div>
       <div class="font-weight-bold">
-        <a href="{{ route('users.show', ['name' => $article->user->name]) }}" class="text-dark">
+        <a href="{{ route('users.show', ['name' => $comment->user->name]) }}" class="text-dark">
           {{ $comment->user->name }}
         </a>
       </div>
